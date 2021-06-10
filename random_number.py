74cb75d6f3a89151d57ebdf6bcd932e237e3faee
CaptainVision <60516434+CaptainVision@users.noreply.github.com>
2021-06-10 11:50:02 +0200

    Add files via upload

---
+++ random_number.py
@@ -0,0 +1,42 @@
+import asyncio
+from datetime import datetime
+
+import discord
+from discord.ext import commands
+
+
+class Commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @bot.command()
+    async def nummer(self, ctx):  # Zufallszahl zwischen 1 und 1000
+        mess = await ctx.send('Deine Zufallszahl wird generiert!')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert! .')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert! ..')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert! ...')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert!')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert! .')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert! ..')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert! ...')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert!')
+        await asyncio.sleep(0.51)
+        await mess.edit(content='Deine Zufallszahl wird generiert! .')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert! ..')
+        await asyncio.sleep(0.5)
+        await mess.edit(content='Deine Zufallszahl wird generiert! ...')
+        await asyncio.sleep(3)
+        await mess.edit(content='Deine Zufallszahl ist ``{}``'.format(random.randint(1, 1000)))
+
+
+def setup(bot):
+    bot.add_cog(Commands(bot))
